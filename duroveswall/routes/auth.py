3b8c06886124bf494fccda086402604b8341aad5
Kotly <george.nozdrin@yandex.ru>
2024-11-08 14:09:15 +0300

    Init

---
+++ duroveswall/routes/auth.py
@@ -0,0 +1,75 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Body, Depends, HTTPException, Request
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from duroveswall.db import get_session
+from duroveswall.schemas import RegistrationForm, RegistrationSuccess
+from duroveswall.utils.user import register_user, authenticate_user
+
+
+api_router = APIRouter(
+    prefix="/user",
+    tags=["User"],
+)
+
+security = HTTPBasic()
+
+@api_router.post(
+    "/authentication",
+    status_code=status.HTTP_200_OK)
+async def authentication(
+    _:Request,
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
+    session: AsyncSession = Depends(get_session),
+): 
+    user = await authenticate_user(session, credentials.username, credentials.password)
+    if not user:
+        raise HTTPException (
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return True
+
+
+@api_router.post(
+    "/registration",
+    status_code=status.HTTP_201_CREATED,
+    response_model=RegistrationSuccess,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Bad parameters for registration",
+        },
+    },
+)
+async def registration(
+    _: Request,
+    registration_form: RegistrationForm = Body(...),
+    session: AsyncSession = Depends(get_session),
+):
+    is_success, message = await register_user(session, registration_form)
+    if is_success:
+        return {"message": message}
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=message,
+    )
+
+@api_router.get(
+    "/me",
+    status_code=status.HTTP_200_OK)
+async def get_me():
+    # FIXME: your code here
+    pass
+
+
+@api_router.delete(
+    "/delete",
+    status_code=status.HTTP_204_NO_CONTENT)
+async def delete():
+    # FIXME: your code here
+    pass
---
+++ duroveswall/utils/user/security.py
@@ -0,0 +1,24 @@
+from duroveswall.config import get_settings
+from .database import get_user
+from sqlalchemy.ext.asyncio import AsyncSession
+from duroveswall.config import get_settings
+from duroveswall.models import User
+
+async def authenticate_user (
+    session: AsyncSession,
+    username: str,
+    password: str,
+) -> User | None:
+    user = await get_user(session, username)
+    if not user:
+        return None
+    if not verify_password(password, user.password):
+        return None
+    return user
+
+def verify_password(
+    plain_password: str,
+    hashed_password: str,
+):
+    pwd_context = get_settings().PWD_CONTEXT
+    return pwd_context.verify(plain_password, hashed_password)
